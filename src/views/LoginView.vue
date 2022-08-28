c0103a7a544e5603043da3fefaa3d047af313bf2
Sean <seanskg72@gmail.com>
2022-08-28 18:39:30 +1000

    template working

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>Login</h1>
+    <h4>
+      asdfasdf
+    </h4>
+    <box>
+      asdf
+    </box>
   </div>
 </template>
 
 <style>
+.container {
+  display: flex;
+  height: 85vh;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.container h1 {
+  color: var(--vt-c-green);
+  margin-bottom: 5px;
+}
+
+.container h4 {
+  margin-bottom: 10px;
+}
+
+.container box {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 20px;
+  border-color: black;
+  height: 450px;
+  width: 80vw;
+  max-width: 400px;
+  color: var(--vt-c-green);
+}
+
 @media (min-width: 1024px) {
-  .about {
+  .container {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+  }
+
+  .container box {
+    width: 40vw;
+    max-width: 500px;
   }
 }
 </style>
